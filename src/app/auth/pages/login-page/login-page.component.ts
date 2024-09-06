340ce4ac96a6b862e3c13575608ea12a20eb7c98
Marigen Rivero <marigen@gmail.com>
2024-09-06 18:28:14 +0200

    🚧 PROGRESS: create form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
+  //private _fb = inject(FormBuilder);
+  constructor(private _fb: FormBuilder) {}
+  public loginForm: FormGroup = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log(this.loginForm.value);
   }
-
 }
